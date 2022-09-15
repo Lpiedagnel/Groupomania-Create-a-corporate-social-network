e260d8f7177f1f29ec823f05e8adfc7ab9a874ac
Lpiedagnel <piedagnel.dev@gmail.com>
2022-09-15 11:25:19 +0200

    Finish Navbar and install font awesome

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import logo from '../images/icon-left-font-monochrome-white.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header () {
 
-    function toggleNavbar() {
-        console.log("Hello world")
-        const x = document.getElementById('nav')
-        if (x.className === 'nav') {
-            x.className += " responsive"
-        } else {
-            x.className = "nav"
-        }
-        console.log(x.className)
-    }
+    function classToggle() {
+        const navs = document.querySelectorAll('.navbar__items')
+        navs.forEach(nav => nav.classList.toggle('navbar__toggleShow'));
+      }
 
     return (
-        <div>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <nav className='nav' id="nav">
-                {/* <Link to="/" className='nav__link'>
-                    <img className='nav__logo' src={logo} />
-                </Link> */}
-                <Link to="/" className="nav__link"><h1 className='nav__brand'>Groupomania</h1></Link>
-                <Link to="/" className='nav__link'>Accueil</Link>
-                <Link to="/signup" className='nav__link'>S'inscrire</Link>
-                <Link to="/login" className='nav__link'>Se connecter</Link>
-                <Link to="#" className='nav__link nav__icon' onClick={toggleNavbar}><i className='fa fa-bars'></i></Link>
-            </nav>
-        </div>
+    <div className="navbar">
+        <Link to="/" className='navbar__link'><img src={logo} className="navbar__logo" /></Link>
+        <FontAwesomeIcon onClick={classToggle} icon={faBars} className="navbar__link navbar__link--toggle" />
+        <nav className="navbar__items">
+            <Link to="/" className='navbar__link'>Accueil</Link>
+            <Link to="/signup" className='navbar__link'>S'inscrire</Link>
+            <Link to="/login" className='navbar__link'>Se connecter</Link>
+        </nav>
+    </div>
     )
 }
 
