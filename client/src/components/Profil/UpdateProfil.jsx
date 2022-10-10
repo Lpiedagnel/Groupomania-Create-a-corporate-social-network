764b3c4eb6ff4bcdb7c35f3ed6455baa552e5420
Lucas Piedagnel <piedagnel.dev@gmail.com>
2022-10-10 17:56:46 +0200

    Finish follow handler on profil page

--- client/src/components/Profil/UpdateProfil.jsx
+++ client/src/components/Profil/UpdateProfil.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import UploadImg from "./UploadImg"
 import { useState } from "react"
 import { updateBio, updateJob } from "../../actions/user.actions"
-import { dateParser } from "../Utils"
+import { dateParser, isEmpty } from "../Utils"
 import FollowHandler from "./FollowHandler"
 
 const UpdateProfil = () => {
@@ -14,8 +14,6 @@ const UpdateProfil = () => {
   const userData = useSelector((state) => state.userReducer)
   const usersData = useSelector((state) => state.usersReducer)
   const dispatch = useDispatch()
-  const [followingPopup, setFollowingPopup] = useState(false)
-  const [followersPopup, setFollowersPopup] = useState(false)
 
   const handleBioUpdate = () => {
     dispatch(updateBio(userData._id, bio))
@@ -44,11 +42,15 @@ const UpdateProfil = () => {
         </div>
 
         <div className="profil__right-part">
-        <div className="profil__job">
-            <h3 className="profil__title-section">Poste au sein de Groupomania</h3>
+          <div className="profil__job">
+            <h3 className="profil__title-section">
+              Poste au sein de Groupomania
+            </h3>
             {updateJobForm === false && (
               <>
-                <p onClick={() => setUpdateJobForm(!updateJobForm)}>{userData.job}</p>
+                <p onClick={() => setUpdateJobForm(!updateJobForm)}>
+                  {userData.job}
+                </p>
                 <button
                   className="profil__btn btn"
                   onClick={() => setUpdateJobForm(!updateJobForm)}
@@ -78,7 +80,9 @@ const UpdateProfil = () => {
             <h3 className="profil__title-section">Bio</h3>
             {updateBioForm === false && (
               <>
-                <p onClick={() => setUpdateBioForm(!updateBioForm)}>{userData.bio}</p>
+                <p onClick={() => setUpdateBioForm(!updateBioForm)}>
+                  {userData.bio}
+                </p>
                 <button
                   className="profil__btn btn"
                   onClick={() => setUpdateBioForm(!updateBioForm)}
@@ -107,96 +111,82 @@ const UpdateProfil = () => {
           <h4 className="profil__info">
             Membre depuis le : {dateParser(userData.createdAt)}
           </h4>
-          <h5
-            className="profil__info"
-            onClick={() => setFollowingPopup(true)}
-          >
+          <h5 className="profil__info">
             Abonnement : {userData.following ? userData.following.length : ""}
           </h5>
-          <h5
-            className="profil__info"
-            onClick={() => setFollowersPopup(true)}
-          >
+          <h5 className="profil__info">
             Abonnés : {userData.followers ? userData.followers.length : ""}
           </h5>
         </div>
       </div>
-      {/* PAUSE ICI */}
-      {followingPopup && (
-        <div className="popup-profil-container">
-          <div className="modal">
-            <h3>Abonnements</h3>
-            <span
-              className="cross"
-              onClick={() => setFollowingPopup(false)}
-            >
-              &#10005;
-            </span>
-            <ul>
-              {usersData.map((user) => {
-                for (let i = 0; i < userData.following.length; i++) {
-                  if (user._id === userData.following[i]) {
-                    return (
-                      <li key={user._id}>
-                        <img
-                          src={user.picture}
-                          alt="user-pic"
+      { !isEmpty(userData.following) ?
+      <div className="profil__follow">
+        <h3 className="profil__title-section">Abonnements</h3>
+        <ul className="profil__list">
+          {usersData.map((user) => {
+            for (let i = 0; i < userData.following.length; i++) {
+              if (user._id === userData.following[i]) {
+                return (
+                  <li
+                    key={user._id}
+                    className="profil__item"
+                  >
+                    <img
+                      className="profil__avatar-follow"
+                      src={user.picture}
+                      alt="user-pic"
+                    />
+                    <h4 className="profil__username-follow">
+                      {user.firstName} {user.lastName}
+                    </h4>
+                    <div className="follow-handler">
+                      <FollowHandler
+                        idToFollow={user._id}
+                        type={"suggestion"}
+                      />
+                    </div>
+                  </li>
+                )
+              }
+            }
+          })}
+        </ul>
+      </div>
+      : null }
+      {!isEmpty(userData.followers) ? (
+        <div className="profil__follow">
+          <h3 className="profil__title-section">Abonnés</h3>
+          <ul className="profil__list">
+            {usersData.map((user) => {
+              for (let i = 0; i < userData.followers.length; i++) {
+                if (user._id === userData.followers[i]) {
+                  return (
+                    <li
+                      key={user._id}
+                      className="profil__item"
+                    >
+                      <img
+                        className="profil__avatar-follow"
+                        src={user.picture}
+                        alt="user-pic"
+                      />
+                      <h4 className="profil__username-follow">
+                        {user.firstName} {user.lastName}
+                      </h4>
+                      <div className="profil__follow-handler">
+                        <FollowHandler
+                          idToFollow={user._id}
+                          type={"suggestion"}
                         />
-                        <h4>{user.pseudo}</h4>
-                        <div className="follow-handler">
-                          <FollowHandler
-                            idToFollow={user._id}
-                            type={"suggestion"}
-                          />
-                        </div>
-                      </li>
-                    )
-                  }
+                      </div>
+                    </li>
+                  )
                 }
-                return null
-              })}
-            </ul>
-          </div>
+              }
+            })}
+          </ul>
         </div>
-      )}
-
-      {followersPopup && (
-        <div className="popup-profil-container">
-          <div className="modal">
-            <h3>Abonnés</h3>
-            <span
-              className="cross"
-              onClick={() => setFollowersPopup(false)}
-            >
-              &#10005;
-            </span>
-            <ul>
-              {usersData.map((user) => {
-                for (let i = 0; i < userData.followers.length; i++) {
-                  if (user._id === userData.followers[i]) {
-                    return (
-                      <li key={user._id}>
-                        <img
-                          src={user.picture}
-                          alt="user-pic"
-                        />
-                        <h4>{user.pseudo}</h4>
-                        <div className="follow-handler">
-                          <FollowHandler
-                            idToFollow={user._id}
-                            type={"suggestion"}
-                          />
-                        </div>
-                      </li>
-                    )
-                  }
-                }
-                return null
-              })}
-            </ul>
-          </div>
-        </div>
-      )}
+      ) : null}
     </div>
   )
 }
